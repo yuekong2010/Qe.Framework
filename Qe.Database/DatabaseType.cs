9b6e83f87fc6874db5f66c18e5714691873d1add
zhujia <1259416820@qq.com>
2018-09-15 16:36:08 +0800

    增加SqlServer操作类

---
+++ Qe.Database/DatabaseType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qe.Database
+{
+    /// <summary>
+    /// 数据库类型
+    /// </summary>
+    public enum DatabaseType
+    {  /// <summary>
+       /// SqlServer
+       /// </summary>
+        SqlServer,
+        /// <summary>
+        /// MySql
+        /// </summary>
+        MySql,
+        /// <summary>
+        /// Oracle
+        /// </summary>
+        Oracle
+    }
+}
---
+++ Qe.Util/Database/FieldValueParam.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qe.Util
+{
+    public class FieldValueParam
+    {
+        /// <summary>
+        /// 字段名称
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 数据值
+        /// </summary>
+        public string value { get; set; }
+        /// <summary>
+        /// 数据类型
+        /// </summary>
+        public int type { get; set; }
+    }
+}
